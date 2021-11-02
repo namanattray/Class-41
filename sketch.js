f3d9fbe6cb3599906e3a0188b367e1997a102e82
namanattray <91533650+namanattray@users.noreply.github.com>
2021-11-02 19:00:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,48 +1,30 @@
-var canvas;
-var backgroundImage, bgImg, car1_img, car2_img, track;
-var database, gameState;
-var form, player, playerCount;
-var allPlayers, car1, car2,fuels, powerCoins;
-var cars = [];
-var fuelImage,lifeImage , powerCoinImage;
-//BP
-function preload() {
-  backgroundImage = loadImage("assets/background.png");
-  car1_img = loadImage("assets/car1.png");
-  car2_img = loadImage("assets/car2.png");
-  track = loadImage("assets/track.jpg");
-  fuelImage = loadImage("assets/fuel.png");
-  powerCoinImage = loadImage("assets/goldCoin.png");
-  lifeImage = loadImage("assets/life.png");
+var canvas, backgroundImage;
 
-}
+var gameState = 0;
+var contestantCount;
+var allContestants;
+var answer;
+var database;
 
-//BP
-function setup() {
-  canvas = createCanvas(windowWidth, windowHeight);
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
   database = firebase.database();
-  game = new Game();
-  game.getState();
-  game.start();
- 
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
 }
 
-//BP
-function draw() {
-  background(backgroundImage);
-  if (playerCount === 2) {
-    game.update(1);
-  }
 
-  if (gameState === 1) {
-    game.play();
+function draw(){
+  background("pink");
+  if(contestantCount === 2){
+    quiz.update(1);
   }
-  if (gameState === 2) {
-    game.showLeaderboard();
-    //game.end();
+  if(gameState === 1){
+    clear();
+    quiz.play();
   }
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
 }
